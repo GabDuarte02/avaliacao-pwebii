60a0bceeb4a3c188311299831025b23d1fb2630d
GabDuarte02 <gabriel.sduarte.oli@gmail.com>
2025-10-14 14:43:12 -0300

    index.js 2.0
    mudança de tema: fábrica para locadora

--- servidor/index.js
+++ servidor/index.js
@@ -1,101 +1,54 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-
-let operarios = [
-    {
-        id: 12345,
-        nome: 'Jucenal',
-        dataNascimento: '15.05.2002',
-        sexo: 'masculino'
+function cadastraFilme() {
+    return {
+        titulo: '',
+        diretor: '',
+        genero: '',
+        dataLocacao: 0,
+        dataDevolucao: 0
     }
-];
+};
 
-//mensagem inicial
+function cadastraCliente () {
+    return {
+        id: 0,
+        cpf: 0,
+        nome: '',
+        loacao: {
+            alugados: [],
+            pendencias: {
+                multa: 0,
+                itensAtrasados: []
+            }
+        },
+    }
+};
+
+function cadastraFornecedor () {
+    return {
+        id: 0,
+        nome: '',
+        contato: 0
+    }
+};
+
+let acervoFilmes = [];
+
+const maquinaMortifera = cadastraFilme();
+maquinaMortifera.titulo = 'Máquina Mortífera'
+maquinaMortifera.diretor = 'Richard Donner'
+maquinaMortifera.genero = 'Ação'
+
+acervoFilmes.push(maquinaMortifera)
+
 app.get('/', (req, res) => {
-    res.send('Banco de funcionários da Metalúrgica Cruz & Cia.')
-})
-
-//mostra a lista de operários ativos na fábrica
-app.get('/operarios', (req, res) => {
-    res.json(operarios);
+    res.send('SITE: LOCADORA SHOE-LEATHER')
 });
 
-app.get('/operarios/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const operario = operarios.find(o => o.id === id);
-
-    if (!operario) {
-        return res.status(404).send('Operário não encontrado.');
-    } else
-
-        res.json(operario)
-});
-
-//cadastramos uma nova operária
-app.post('/operarios', (req, res) => {
-    const novoOperario = {
-        id: 56789,
-        nome: 'Ana Silva',
-        dataNascimento: '10.10.2003',
-        sexo: 'feminino'
-    };
-    operarios.push(novoOperario);
-    res.status(201).json(novoOperario);
-});
-
-app.put('/operarios/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = operarios.findIndex(o => o.id === id);
-
-    if (index === -1) {
-        return res.status(404).send('Operário não encontrado.');
-    }
-
-    //precisamos atualizar o objeto de ID 12345, que está sem sobrenome e sua data está incorreta
-    const operarioAtualizado = {
-        id: 12345,
-        nome: 'Jucenal Santos',
-        dataNascimento: '01.01.2003',
-        sexo: 'masculino'
-    };
-
-    operarios[index] = operarioAtualizado;
-    res.json(operarioAtualizado);
-});
-app.patch('/operarios/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = operarios.findIndex(o => o.id === id);
-
-    if (index === -1) {
-        return res.status(404).send('Operário não encontrado.');
-    }
-
-    const operarioExistente = operarios[index];
-    const dadosParaAtualizar = req.body;
-
-    const operarioAtualizado = {
-        ...operarioExistente, 
-        ...dadosParaAtualizar
-    };
-    
-    operarioAtualizado.id = id;
-
-    operarios[index] = operarioAtualizado;
-    res.json(operarioAtualizado);
-});
-//simula o desligamento de um funcionário
-app.delete('/operarios/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = operarios.findIndex(o => o.id === id);
-
-    if (index === -1) {
-        return res.status(404).send('Operário não encontrado.');
-    }
-
-    operarios.splice(index, 1);
-    res.status(200).send('Operário deletado com sucesso.');
+app.get('/acervo', (req, res) => {
+    res.json(acervoFilmes)
 });
 
 const port = 3000;
